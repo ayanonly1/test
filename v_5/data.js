f7f01586d74778fa204ca5281488f57f7493874f
sguha-fc <sahasrangshu@fusioncharts.com>
2015-07-14 19:27:58 +0530

    changes

--- v_5/data.js
+++ v_5/data.js
@@ -29,5 +29,36 @@ var dataStore = {
                 }
             }],
             data: []
+        },
+        "employee": {
+            fields: [{
+                name: "Customer Name",
+                dataGenerator: lib.generateName,
+            }, {
+                name: "Customer Email",
+                dataGenerator: lib.generateEmail,
+            }, {
+                name: "Order Date",
+                dataGenerator: function() {
+                    var date = "";
+                    date += lib.generateRandomNumber(1, 30);
+                    date += "/";
+                    date += lib.generateRandomNumber(1, 12);
+                    date += "/";
+                    date += lib.generateRandomNumber(2010, 2016);
+                    return date;
+                }
+            }, {
+                name: "Order Amount",
+                dataGenerator: lib.genRandomNoGenerator(1, 10000, 2)
+            }, {
+                name: "Profit",
+                arg: "Order Amount",
+                dataGenerator: function(orderAmount) {
+                    orderAmount = orderAmount || 10;
+                    return lib.generateRandomNumber(-orderAmount / 4, orderAmount / 2, 2);
+                }
+            }],
+            data: []
         }
     };
