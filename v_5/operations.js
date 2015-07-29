1a3c1ab595a7d4bc0020f50b227f0868490f0319
Ayan Pal <ayanonly1@gmail.com>
2015-07-29 18:26:11 +0530

    Added ned data structure for the UI operations

--- v_5/operations.js
+++ v_5/operations.js
@@ -1,28 +1,56 @@
 /**
-* This file holds list of all the operations
-* Every operation will have the following attributes
-* config: this attribute holds the priority wise dependency list for the operation
-*/
+ * This file holds list of all the operations
+ * Every operation will have the following attributes
+ * config: this attribute holds the priority wise dependency list for the operation
+ */
 var operationArray = {
     sort: {
-        config: ['table', 'field'],
+        config: [{
+            name: 'table',
+            methodName: "table",
+            displayText: "Please select the table"
+        }, {
+            name: 'field',
+            methodName: "field",
+            displayText: "Please select the Field of the table"
+        }, {
+            name: "order",
+            methodName: "list",
+            displayText: "Please select the order",
+            value: ["DESC", "ASC"]
+        }],
         click: function() {
             var operationParam = {},
-            index=0,
-            callBack=function(){
-                if(index==operationArray.sort.config.length-1) {
-                    operationArray.sort.operation(operationParam);
-                } else {
-                    operationParam[operationArray.sort.config[index]] = document.getElementById("txt_"+operationArray.sort.config[index] + "_" + index).value;
+                index = 0,
+                thisOp = operationArray.sort,
+                localConfig = thisOp.config,
+                config_i = localConfig[index]
+            callBack = function() {
+                if (index < localConfig.length) {
+                    operationParam[config_i.name] = document.getElementById("txt_" + config_i.name + "_" + index).value;
                     index += 1;
-                    ui["create_" + operationArray.sort.config[index]](operationParam, operationArray.sort.config[index] + "_" + index);
+                    config_i = localConfig[index];
+                    if (index == localConfig.length) {
+                        operationArray.sort.operation(operationParam);
+                    } else {
+                        // put the comment
+                        ui.update_comment(config_i.displayText);
+                        // create the input
+                        ui["create_" + config_i.methodName](operationParam, config_i.name + "_" + index, config_i.value);
+                    }
+                } else {
+                    operationArray.sort.operation(operationParam);
                 }
             };
-
-            ui["create_" + operationArray.sort.config[index]](operationParam, operationArray.sort.config[index] + "_" + index);
-
+            // clear the ui
+            ui.clear_ui();
+            // put the comment
+            ui.update_comment(config_i.displayText);
+            // create the first input
+            ui["create_" + config_i.methodName](operationParam, config_i.name + "_" + index, config_i.value);
+            // create the button
             ui.create_button(callBack);
-            
+
         },
         operation: function(config) {
 
@@ -39,6 +67,8 @@ var operationArray = {
     },
     search: {
         click: function() {
+            // clear the ui
+            ui.clear_ui();
             operationArray.search.operation();
         },
         operation: function(config) {
@@ -54,6 +84,8 @@ var operationArray = {
     },
     serialize: {
         click: function() {
+            // clear the ui
+            ui.clear_ui();
             operationArray.serialize.operation();
         },
         operation: function(config) {
--- v_5/ui.js
+++ v_5/ui.js
@@ -1,44 +1,54 @@
 var ui = {
-	customList: function(options, id) {
-		var select = document.createElement('select'),
-		option;
-		select.setAttribute('id', "txt_"+id);
-		for(var optionIndex=0; optionIndex<options.length; optionIndex++) {
-			option = document.createElement('option');
-			option.setAttribute('value', options[optionIndex]);
-			option.appendChild(document.createTextNode(options[optionIndex]));
-			select.appendChild(option);
-		}
-		select.setAttribute('class', 'op_select');
-		if(document.getElementById('btn_go')) {
-			var btnElement = document.getElementById('btn_go');
-			btnElement.parentNode.insertBefore(select, btnElement);
-		} else {
-			document.getElementById("div_operation").appendChild(select);
-		}
-	},
-	create_table: function(config, id) {
-		var options = [];
-		for(var index in dataStore) {
-			options.push(index);
-		}
-		ui.customList(options, id);
-	},
-	create_field: function(config, id) {
-		tableName = config.table || "";
-		var fields = [];
-		for(var index=0; index<dataStore[tableName].fields.length; index++) {
-			fields.push(dataStore[tableName].fields[index].name);
-		}
-		ui.customList(fields, id);
-	},
-	create_button: function(callBack){
-		var button = document.createElement('input');
-		button.setAttribute('type', 'button');
-		button.setAttribute('value', "Go");
-		button.setAttribute('id', "btn_go");
-		button.addEventListener('click', callBack);
-		document.getElementById("div_operation").appendChild(button);	
-	}
-	
+    create_list: function(config, id, options) {
+        var select = document.createElement('select'),
+            option;
+        select.setAttribute('id', "txt_" + id);
+        for (var optionIndex = 0; optionIndex < options.length; optionIndex++) {
+            option = document.createElement('option');
+            option.setAttribute('value', options[optionIndex]);
+            option.appendChild(document.createTextNode(options[optionIndex]));
+            select.appendChild(option);
+        }
+        select.setAttribute('class', 'op_select');
+        if (document.getElementById('btn_go')) {
+            var btnElement = document.getElementById('btn_go');
+            btnElement.parentNode.insertBefore(select, btnElement);
+        } else {
+            document.getElementById("div_operation").appendChild(select);
+        }
+    },
+    create_table: function(config, id) {
+        var options = [];
+        for (var index in dataStore) {
+            options.push(index);
+        }
+        ui.create_list({}, id, options);
+    },
+    create_field: function(config, id) {
+        tableName = config.table || "";
+        var fields = [];
+        for (var index = 0; index < dataStore[tableName].fields.length; index++) {
+            fields.push(dataStore[tableName].fields[index].name);
+        }
+        ui.create_list({}, id, fields);
+    },
+    create_button: function(callBack) {
+        var button = document.createElement('input');
+        button.setAttribute('type', 'button');
+        button.setAttribute('value', "Go");
+        button.setAttribute('id', "btn_go");
+        button.addEventListener('click', callBack);
+        document.getElementById("div_operation").appendChild(button);
+    },
+    update_comment: function(comment) {
+    	comment == typeof comment === "undefined" ? "" : comment;
+    	document.getElementById("div_operation_comment").innerHTML = comment;
+    },
+    clear_ui: function() {
+        var container = document.getElementById("div_operation");
+        while (container.firstChild) {
+            container.removeChild(container.firstChild);
+        }
+    }
+
 }
